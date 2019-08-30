c3d32bcd6ddafeb637c120fdd7ab6acb27e6f6bf
Hao Jiang <hjian8@uwo.ca>
2019-08-30 14:16:37 -0400

    pass test

--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 	"os"
 	"log"
-	"fmt"
 )
 
 //var wg sync.WaitGroup
@@ -16,12 +15,9 @@ import (
 func sumWorker(nums chan int, out chan int) {
 	// TODO: implement me
 	// HINT: use for loop over `nums`
-	//defer wg.Done()
-	fmt.Println("bbb")
 	sum := 0
 	for v := range nums {
 		sum += v
-		fmt.Println("sum: ", sum)
 	}
 	
 	out <- sum 
@@ -36,8 +32,6 @@ func sum(num int, fileName string) int {
 	// TODO: implement me
 	// HINT: use `readInts` and `sumWorkers`
 	// HINT: used buffered channels for splitting numbers between workers
-	//wg.Add(num)
-	
 	reader, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -47,31 +41,24 @@ func sum(num int, fileName string) int {
 		log.Fatal(err)
 	}
 	
-	nums := make(chan int, num)
+	nums := make(chan int, len(arr) / num)
 	out := make(chan int)
-	fmt.Println("num: , len: ", num, len(arr))
-
+	
 	for i := 0; i < num; i++ {
-		fmt.Println("i: ", i)
 		go sumWorker(nums, out)
-
 	}
 	
-	for v := range arr {
-		fmt.Println("v: ", v)
+	for _, v := range arr {
 		nums <- v
-
 	}
 
-	fmt.Println("aaa")
 	close(nums)
-	//wg.Wait()
-	
 	
 	var sum int
 	for i:= 0; i < num; i++ {
 		sum += <-out
 	}
+
 	return sum
 	
 	
