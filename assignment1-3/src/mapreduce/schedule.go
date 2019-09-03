31a3919abadd38b8d6415c75eb4a14ede151d422
haojiang1029 <hjian8@uwo.ca>
2019-09-03 15:34:51 -0400

    assgn3

---
+++ assignment1-3/src/mapreduce/schedule.go
@@ -0,0 +1,60 @@
+package mapreduce
+
+import (
+	"log"
+	"sync"
+)
+
+// schedule starts and waits for all tasks in the given phase (Map or Reduce).
+func (mr *Master) schedule(phase jobPhase) {
+	var ntasks int
+	var nios int // number of inputs (for reduce) or outputs (for map)
+	switch phase {
+	case mapPhase:
+		ntasks = len(mr.files)
+		nios = mr.nReduce
+	case reducePhase:
+		ntasks = mr.nReduce
+		nios = len(mr.files)
+	}
+
+	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
+
+	// All ntasks tasks have to be scheduled on workers, and only once all of
+	// them have been completed successfully should the function return.
+	// Remember that workers may fail, and that any given worker may finish
+	// multiple tasks.
+	//
+	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+	//
+	var waitGroup sync.WaitGroup
+	for i:= 0; i < ntasks; i++ {
+		waitGroup.Add(1)
+		var taskArgs DoTaskArgs
+		taskArgs.JobName = mr.jobName
+		taskArgs.Phase = phase
+		taskArgs.TaskNumber = i
+		taskArgs.NumOtherPhase = nios
+		if phase == mapPhase {
+			taskArgs.File = mr.files[i]
+		}
+		go func() {
+			defer waitGroup.Done()
+			
+			worker := <- mr.registerChannel
+			if !call(worker, "Worker.DoTask", &taskArgs, nil) {
+				log.Fatal("call RPC failed")
+			}
+			go func() {
+				mr.registerChannel <- worker
+			} ()
+			
+		} ()
+	}
+	waitGroup.Wait()
+
+
+
+
+	debug("Schedule: %v phase done\n", phase)
+}
